ea833be66aa916f8a633e895dd782a8b21d709ec
ruiyang <ruiyang@dell.com>
2020-07-30 18:45:47 +0800

    fix getQ.pl return job_cnt=0 bug on child node by getQ.py

---
+++ bin/getQ.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os
+import time
+USER="sry"
+MYMAX=80     ##change this to the maximum number of jobs by you
+TOTALMAX=384  ##change this to the maximum number of jobs by all users
+INTERVAL=10
+while True:
+    jobs = int(os.popen('/usr/local/bin/qstat -u {USER} |wc -l'.format(USER=USER)).readline())
+    total_jobs = int(os.popen('/usr/local/bin/qstat |wc -l').readline())
+    print(jobs)
+    if jobs <= MYMAX:
+        break
+    elif total_jobs < TOTALMAX:
+        break
+    else:
+        time.sleep(INTERVAL)
--- run_CAGI.py
+++ run_CAGI.py
@@ -4,7 +4,7 @@ import pandas as pd
 aa_123dict = {'A': 'ALA', 'R': 'ARG', 'N': 'ASN', 'D': 'ASP', 'C': 'CYS', 'Q': 'GLN', 'E': 'GLU', 'G': 'GLY', 'H': 'HIS', 'I': 'ILE',
               'L': 'LEU', 'K': 'LYS', 'M': 'MET', 'F': 'PHE', 'P': 'PRO', 'S': 'SER', 'T': 'THR', 'W': 'TRP', 'Y': 'TYR', 'V': 'VAL'}
 app='/public/home/sry/DMBS/src/bin/DMBS.sh'
-queue='/public/home/sry/DMBS/src/bin/getQ.pl'
+queue='/public/home/sry/DMBS/src/bin/getQ.py'
 outdir = '/public/home/sry/DMBS/output'
 CAGI_dir = '/public/home/sry/DMBS/dataset/CAGI'
 seq_lst_pth = '{CAGI_dir}/seq.lst'.format(CAGI_dir=CAGI_dir)
