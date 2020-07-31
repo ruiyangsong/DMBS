3bff3446d39249080e197f08f13ac2abc9dff0bb
ruiyang <ruiyang@dell.com>
2020-08-01 00:30:22 +0800

    sync

--- bin/getQ_master.py
+++ bin/getQ_master.py
@@ -5,15 +5,16 @@ if len(sys.argv) == 1:
     print("Usage: {filename} [tag_prefix]".format(filename=sys.argv[0]))
     exit(0)
 
-JOBTAGPRE=sys.argv[1] # prefix of special Job_Names that you want to monitor
+PATTERN=sys.argv[1] # pattern of the master Job_Names that you want to monitor
 
 USER="sry"
 MYMAX=80     ##change this to the maximum number of jobs by you
-TOTALMAX=384  ##change this to the maximum number of jobs by all users
+TOTALMAX=484  ##change this to the maximum number of jobs by all users
 INTERVAL=30
-JOBPERNODE=2  ##change this to the maximum number of special jobs by all users on each node
 
-## queue overall jobs
+MASTERMAX=2  ##change this to the maximum number of master jobs by you
+
+## Queue overall jobs
 while True:
     jobs = int(os.popen('/usr/local/bin/qstat -u {USER} |wc -l'.format(USER=USER)).readline())
     total_jobs = int(os.popen('/usr/local/bin/qstat |wc -l').readline())
@@ -24,24 +25,11 @@ while True:
     else:
         time.sleep(INTERVAL)
 
-## queue special jobs
+## Queue special jobs. They condition is that submitting Master jobs no more than MASTERMAX
 while True:
-    pattern = re.compile(JOBTAGPRE)
-    # up_nodes  = [line.split()[0] for line in os.popen('/usr/local/bin/pbsnodes -l up').readlines()]
-    free_nodes = [line.split()[0] for line in os.popen('/usr/local/bin/pbsnodes -l free').readlines()]
-
-    info_str  = os.popen('pbsnodes -a | grep -E "^node| jobs"').readlines()
-    for idx in range(len(info_str)):
-        if idx % 2 == 0:
-            node  = info_str[idx].strip()
-            if node in free_nodes:
-                ids   = [id.split('/')[-1].strip() for id in info_str[idx+1].split(',')]
-                names = [os.popen('/usr/local/bin/qstat -f {id} | grep Job_Name'.format(id=id)).readline().split('=')[-1].strip() for id in ids]
-                if len(pattern.findall('.'.join(names))) < JOBPERNODE:
-                    print(node)
-                    exit(0)
-    time.sleep(INTERVAL)
-
-    # hostname  = [info_str[idx].strip() for idx in range(len(info_str)) if idx % 2 == 0]
-    # job_ids   = list(map(lambda lst: [id.split('/')[-1].strip() for id in lst], [info_str[idx+1].split(',') for idx in range(len(info_str)) if idx % 2 == 0]))
-    # job_names = list(map(lambda lst: [os.popen('/usr/local/bin/qstat -f {id} | grep Job_Name'.format(id=id)).readline().split('=')[-1].strip() for id in lst], job_ids))
+    pattern = re.compile(PATTERN)#pattern.findall('.'.join(names))
+    job_cnt = int(os.popen('/usr/local/bin/qstat -u {USER} | grep -E "{PATTERN}" | wc -l'.format(USER=USER,PATTERN=PATTERN)).readline())
+    if job_cnt < MASTERMAX:
+        exit(0)
+    else:
+        time.sleep(INTERVAL)
