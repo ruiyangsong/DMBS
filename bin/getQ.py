8f5f6b71b38fb20255a6d702844e180ad2959b44
ruiyang <ruiyang@dell.com>
2020-08-02 19:18:22 +0800

    sync

--- bin/getQ.py
+++ bin/getQ.py
@@ -4,7 +4,7 @@ import time
 USER="sry"
 MYMAX=80     ##change this to the maximum number of jobs by you
 TOTALMAX=384  ##change this to the maximum number of jobs by all users
-INTERVAL=30
+INTERVAL=10
 while True:
     jobs = int(os.popen('/usr/local/bin/qstat -u {USER} |wc -l'.format(USER=USER)).readline())
     total_jobs = int(os.popen('/usr/local/bin/qstat |wc -l').readline())
